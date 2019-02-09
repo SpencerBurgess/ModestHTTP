03ffb02b5ce51816963bc1582fc6f5a015ec0182
Spencer <Spencer@LAPTOP-BU3QNNGB>
2019-02-09 01:16:02 -0700

    added Server Class and basic set up for connections

--- modest/src/main/java/com/kingfancy/modest/Main.java
+++ modest/src/main/java/com/kingfancy/modest/Main.java
@@ -1,5 +1,11 @@
 package com.kingfancy.modest;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Date;
+
+import com.kingfancy.modest.Server;
+
 /**
  * Modest HTTP
  * A simple, yet robust HTTP server.
@@ -8,10 +14,32 @@ package com.kingfancy.modest;
  * Version: 0.01
  * License: GNU GENERAL PUBLIC LICENSE
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+	
+	static final int PORT = 8080;
+	static final boolean verbose = true;
+	
+    public static void main( String[] args ) {
+    	
+    	try {
+			ServerSocket serverConnect = new ServerSocket(PORT);
+			System.out.println("Server started. Listening on port " + PORT);
+			
+			// we listen until user halts server execution
+			while (true) {
+				Server myServer = new Server(serverConnect.accept());
+				
+				if (verbose) {
+					System.out.println(new Date() + " - Connecton opened");
+				}
+				
+				// create dedicated thread to manage each client connection
+				Thread thread = new Thread(myServer);
+				thread.start();
+			}
+			
+		} catch (IOException e) {
+			System.err.println("Connection error : " + e.getMessage());
+		}
     }
 }
