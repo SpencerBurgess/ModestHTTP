444494e5e08779d8dafe63bef8ffae131070516f
Spencer <Spencer@LAPTOP-BU3QNNGB>
2019-02-09 00:44:27 -0700

    init project

---
+++ modest/src/main/java/com/kingfancy/modest/Main.java
@@ -0,0 +1,17 @@
+package com.kingfancy.modest;
+
+/**
+ * Modest HTTP
+ * A simple, yet robust HTTP server.
+ * 
+ * Author: Spencer Burgess
+ * Version: 0.01
+ * License: GNU GENERAL PUBLIC LICENSE
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
